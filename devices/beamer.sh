0a481b747b1075796996c1bd859190f65c507f42
Anne van Rossum <anne@almende.org>
2012-09-25 15:43:08 +0200

    Acroread presentation and beamer magic

---
+++ devices/beamer.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+acroread -geometry 1024x768+1920+0 "$1"
+
+# Actually to make it full screen, drag it to the beamer screen, make it such that it is half-full screen (it occupies the left part of the beamer screen). This can be done by dragging it to the bottom. And only then make it fullscreen.

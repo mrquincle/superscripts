4f91e69d3922eb9bfff60d2423a2c44ecae16d3b
mrquincle <anne@almende.org>
2014-01-27 20:29:37 +0100

    Update script to check extension

--- shell/openpdf.sh
+++ shell/openpdf.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 path=$HOME/setup
-
-# wait indefinitely, do not quit after
-flags="-m --format '%w %f'"
 flags="--format %f"
-
 while true; do
-
-echo "inotifywait $flags -e close_write $path" 
-download=$(inotifywait $flags -e close_write $path)
- 
-echo $download 
-
-mime=$(file -b --mime-type "$path/$download")
-
-if [ "$mime" = "application/pdf" ]; then
-
-	acroread "$path/$download"
-fi
- 
-
+	download=$(inotifywait $flags -e close_write $path)
+	file="$path/$download"
+	extension="${file##*.}"
+	if [ "$extension" != "pdf" ]; then
+		echo "Extension is $extension while pdf is expected"
+		continue
+	fi
+	mime=$(file -b --mime-type "$file")
+	if [ "$mime" = "application/pdf" ]; then
+		echo "Open $file"
+		acroread "$file"
+	fi
 done

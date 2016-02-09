9714d7d2079caec4edba18442acb507635938b73
Anne van Rossum <anne@dobots.nl>
2016-02-09 16:28:09 +0100

    Add script that get all data from sensor in CommonSense

--- sense/get.sh
+++ sense/get.sh
@@ -21,7 +21,7 @@ if [[ "$sensor" == "" ]]; then
 fi
 
 if [[ "$page" == "" ]]; then
-	page=0
+	page=1
 fi
 
 
@@ -40,6 +40,6 @@ mkdir -p $result_folder
 echo "Get page $page from sensor $sensor in account $username"
 echo " # note 1: you can use $cred_file to store username and md5 hash of your password)"
 echo " # note 2: you can expect nothing back from sense to indicate success, navigate to their online database instead"
-curl -s -H "Content-Type: application/json" -X GET "https://api.sense-os.nl/sensors/$sensor/data?username=$username&password=$password&page=$page" -o "$result_folder/$page.txt"
+curl -s -H "Content-Type: application/json" -X GET "https://api.sense-os.nl/sensors/$sensor/data?username=$username&password=$password&page=$page&per_page=1000" -o "$result_folder/$page.txt"
 echo "Result written to $result_folder/$page.txt"
 echo "Done"
---
+++ sense/get_all.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+sensor=${1:? "Usage: $0 sensor_id"}
+
+start=100000
+
+echo "Only get first 100000 pages"
+end=$(( $start + 100000 ))
+
+result_folder=$sensor
+
+counter=$start
+until [ $counter -gt $end ]; do
+	echo "./get.sh $sensor $counter"
+	./get.sh $sensor $counter
+
+	end_reached=$(< $result_folder/$counter.txt grep marpessa)
+
+	if [ -n "$end_reached" ]; then
+		echo "End reached!"
+		exit
+	fi
+	counter=$(( $counter + 1 ))
+done
+

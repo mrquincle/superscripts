bcf39ce0aaa0262db42bff7ec53c07d329ee1b28
Anne van Rossum <anne@crownstone.rocks>
2019-02-25 17:47:54 +0100

    Add scripts

--- mailchimp/create_campaign.sh
+++ mailchimp/create_campaign.sh
@@ -1,1 +1,16 @@
-../../mailchimp/create_campaign.sh
+#!/bin/sh
+
+usage="$0 subject"
+
+mkdir -p tmp
+
+subject=${1:? "$usage"}
+
+echo mc campaigns create --subject=''''"$subject"'''' --from-email='anne@crownstone.rocks' --from-name='Crownstone' --html-filename=campaign.html | tee tmp/log.txt
+mc campaigns create --subject=''''"$subject"'''' --from-email='anne@crownstone.rocks' --from-name='Crownstone' --html-filename='mailchimp.html' | tee tmp/log.txt
+
+campaign_id=$(cat tmp/log.txt | grep 'campaign' | grep 'id' | tr -d ' ' | cut -f2 -d'=')
+
+echo "Campaign id: $campaign_id"
+
+echo $campaign_id > tmp/campaign_id.txt
--- mailchimp/fill_campaign.sh
+++ mailchimp/fill_campaign.sh
@@ -1,1 +1,9 @@
-../../mailchimp/fill_campaign.sh
+#!/bin/sh
+
+usage="$0 input.md"
+ifile=${1:? "$usage"}
+
+mkdir -p tmp
+
+markdown "$ifile" -f markdown > "tmp/content.html"
+sed -e '/{{ contents }}/ {' -e 'r tmp/content.html' -e 'd' -e '}' mailchimp-template.html > campaign.html
